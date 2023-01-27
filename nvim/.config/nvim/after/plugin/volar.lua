8bb2d9d056d7163bf8f853f3a0e6f2f01004d616
Bram <bramswennen1@gmail.com>
2023-01-27 20:45:47 +0100

    enable volar takeover mode

---
+++ nvim/.config/nvim/after/plugin/volar.lua
@@ -0,0 +1,6 @@
+-- Volar can serve as a language server for both Vue and TypeScript via [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471).
+-- To enable Take Over Mode, override the default filetypes in `setup{}` as follows:
+
+require'lspconfig'.volar.setup{
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+}
